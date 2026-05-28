f392e232228c370d971b115aa7b7be92473b8fdc
samarpansarkar <samarpansarkar209@gmail.com>
2026-05-29 01:51:08 +0530

    feat: add JSON configuration, restructure styles, and update main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import { Provider } from "react-redux";
+import App from "./App.jsx";
+import "./styles/global.css";
 
-createRoot(document.getElementById('root')).render(
+let store = [];
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <HelmetProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HelmetProvider>
   </StrictMode>,
-)
+);
